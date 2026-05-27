e536f691a729f695538c2fd6abce50762bf6f3b4
Daltongit <miltonfuel20@gmail.com>
2026-05-27 14:25:34 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,6 @@ window.SUPABASE_URL = "https://gttovykbkktstmdwdkib.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_HnlxxDQaTItY6iDk2ssxaQ_5WKcwGHq";
 
 window.TABLES = {
-  spaces: "spaces",
-  reservations: "reservations"
+  cases: "triage_cases",
+  actions: "triage_actions"
 };
